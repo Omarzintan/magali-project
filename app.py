6574444e800243b6ec3b0a6888fe55fd811a52f2
Omarzintan <zintanomar@gmail.com>
2020-08-22 16:20:57 -0700

    stop using ezsheets

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+from __future__ import print_function
 from flask import Flask, jsonify, request, render_template
 from flask_assets import Bundle, Environment
 import ezsheets
+
+
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1kX_YT4AB2TjMurYKjmkvKrSix0C51lIkaHXBVpbx8r0'
+SAMPLE_RANGE_NAME = 'Sheet1!A2:C'
 
 app = Flask(__name__)
 
@@ -33,8 +48,8 @@ def test_page():
     # look inside `templates` and serve `home.html`
     return render_template('home.html')
 
-
-# Returns data from Spreadsheet as a JSON file to script.js
+'''
+# Returns data from Spreadsheet as a JSON file to script.js using ezsheets
 @app.route('/data')
 def getData():
     results = {}
@@ -56,6 +71,51 @@ def getData():
             j+=1
         i+=1
     return jsonify(results)
+'''
+
+# Returns data from Spreadsheet as a JSON file to script.js using google service
+@app.route('/data1')
+def getData1():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('No data found.')
+        return
+    else:
+        print('Name, Major:')
+        #for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            #print('%s, %s' % (row[0], row[4]))
+        return jsonify(values)
+
 
 if __name__ == '__main__':
     app.run()
