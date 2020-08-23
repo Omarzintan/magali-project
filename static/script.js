5c69a4a8cd05a012161a4e415068049f628f54bd
Omarzintan <zintanomar@gmail.com>
2020-08-22 21:44:34 -0700

    remove main.js file and modify script.js

--- static/script.js
+++ static/script.js
@@ -1,67 +1,17 @@
 function sayHello() {
     alert('Hello!');
-    console.log('Hello');
-}
-
-function fetchHelloText() {
-    fetch('/hello')
-        .then(function (response) {
-            return response.text();
-        }).then(function (text) {
-            console.log('GET response text:');
-            console.log(text); // Print the greeting as text
-        });
-}
-
-function fetchHelloJSON() {
-    fetch('/hello')
-        .then(function (response) {
-            console.log(response);
-            return response.json(); // But parse it as JSON this time
-        })
-        .then(function (json) {
-            console.log('GET response as JSON:');
-            console.log(json); // Here's our JSON object
-        });
-}
-
-// POST to python file
-function postMessage() {
-    fetch ('/hello', {
-        method: 'POST',
-
-        // JSON payload
-        body: JSON.stringify({
-            "greeting": "Hello from the browser!"
-        })
-    }).then(function (response) {
-        return response.text();
-    }).then(function (text) {
-
-        console.log('POST response: ');
-
-        // Should be 'OK'
-        console.log(text);
-    });
 }
 
 /* Gets data from Spreadsheet and displays it */
-function getData() {
-    fetch('/data')
+function getData(category) {
+    fetch('/data?category='+category)
         .then(function(response) {
-            console.log(response);
             return response.json();
         }).then(function(results) {
-            dataDiv = document.getElementById('data');
-            dataDiv.innerHTML = '';
-            for (x in results) {
-                categoryDiv = document.createElement('div');
-                categoryDiv.id = x;
-                categoryList = makeList(results[x]);
-                categoryDiv.innerHTML = '<p>'+x+'</p>';
-                categoryDiv.appendChild(categoryList);
-                dataDiv.appendChild(categoryDiv);
-            }
+            dataDiv = document.getElementById('data-'+category);
+            dataDiv.innerHTML = category;
+            categoryList = makeList(results);
+            dataDiv.appendChild(categoryList);
         });
 }
 
@@ -71,9 +21,8 @@ function makeList(array) {
     for (var i = 0; i < array.length; i++) {
         listElement = document.createElement('li');
         linkElement = document.createElement('a');
-        linkElement.href = array[i];
-        linkElement.innerText = array[i];
-        //listElement.innerHTML = '<a href="'+array[i]+'">'+array[i]+'</a>';
+        linkElement.href = array[i][1];
+        linkElement.innerText = array[i][0];
         listElement.append(linkElement);
         unorderedListElement.appendChild(listElement);
     }
