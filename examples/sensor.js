d6ca3e4e3edc4522388a30614babd2b256ad9d0c
Omarzintan <zintanomar@gmail.com>
2020-08-23 16:58:32 -0700

    clean up git repository

--- examples/sensor.js
+++
@@ -1,9 +0,0 @@
-function reportReading() {
-    // Pick any temperature from a range of -5 to 15
-    const temperature = (Math.random() * 20) - 5;
-    // Write with newline char
-    process.stdout.write(temperature + '\n');
-    // Wait 0 to 5 seconds
-    setTimeout(reportReading, Math.random() * 5000);
-}
-reportReading();
